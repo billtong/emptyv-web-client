1925580d113d1724445c76d3b91b16a0c022e53f
billtong <zhiyuantongbill@gmail.com>
2019-02-27 01:22:43 -0500

    fix comment bug

--- app/api/postComment.jsx
+++ app/api/postComment.jsx
@@ -4,10 +4,8 @@ import { BASE_URL } from './baseURL.jsx';
 
 module.exports = {
   postComment: (inputJson, userToken, sessionId) => {
-    const deviceListURL = `${BASE_URL}api/comment/write?
-    userId=${inputJson.userId}&
-    token=${userToken}&
-    sessionId=${sessionId}`;
+    //这里不可空行，空行会被写入
+    const deviceListURL = `${BASE_URL}api/comment/write?userId=${inputJson.userId}&token=${userToken}&sessionId=${sessionId}`;
     return axios.post(deviceListURL, {
       commentContent: inputJson.commentContent,
       videoId: inputJson.videoId,
