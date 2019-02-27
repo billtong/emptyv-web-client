97a8798d890a38d30b561cd31301688b631d5a04
billtong <zhiyuantongbill@gmail.com>
2019-02-26 23:40:12 -0500

    fix css overrides

--- app/app.jsx
+++ app/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route, Router, IndexRoute, hashHistory } from 'react-router';
-
 import * as configureStore from './store/configureStore.jsx';
 import Main from './components/Main.jsx';
 import SignIn from './components/userSign/SignIn.jsx';
@@ -11,8 +10,6 @@ import Home from './components/Home.jsx'
 import UserPage from './components/userPage/UserPage.jsx'
 import VideoPage from './components/videoPage/VideoPage.jsx'
 import DonatePage from './components/donatePage/DonatePage.jsx';
-
-
 
 //这里通过redux将所有的state打包成了一个json
 const store = configureStore.configure();
