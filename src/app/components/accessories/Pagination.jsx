4b55f1e32a6a6f2abd7f680b2f7eeee4dc7c01e6
billtong <zhiyuantongbill@gmail.com>
2019-03-27 23:00:36 -0400

    pagination css

--- src/app/components/accessories/Pagination.jsx
+++ src/app/components/accessories/Pagination.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GoChevronLeft, GoChevronRight} from 'react-icons/go';
 import { connect } from 'react-redux';
 import { updateVideoPageAction } from '../../actions/getVideoListActions';
 import { updateCommentPageAction } from '../../actions/getCommentListAction';
@@ -29,10 +30,10 @@ class Pagination extends React.Component {
         case 'comment':
           this.props.updateCommentPageAction(1, this.state.sizes);
           break;
-        case 'video' :
+        case 'video':
           this.props.updateVideoPageAction(1, this.state.sizes);
           break;
-        default :
+        default:
           break;
       }
       this.setState(prevState => ({
@@ -42,40 +43,62 @@ class Pagination extends React.Component {
     }
   }
 
+  changePage = (e, index) => {
+    e.preventDefault();
+    this.setState(prevState => ({
+      ...prevState,
+      currPage: index + 1,
+    }));
+    switch (this.props.tag) {
+      case 'comment': 
+        this.props.updateCommentPageAction(index + 1, this.state.sizes);
+        break;
+      case 'video': 
+        this.props.updateVideoPageAction(index + 1, this.state.sizes);
+        break;
+      default :
+        break;
+    }
+    document.documentElement.scrollTop = 0;
+  };
+
   render() {
     const list = this.props.list;
     const sizes = this.state.sizes;
     let pagination = null;
     if(list !== undefined && list.length > 0) {
       pagination = Array(list.length % sizes === 0 ? Math.floor(list.length / sizes) : (Math.floor(list.length / sizes)) + 1).fill(0);
+
       pagination = pagination.map((value, index) => {
-        const changePage = (e) => {
-          e.preventDefault();
-          this.setState(prevState => ({
-            ...prevState,
-            currPage: index + 1,
-          }));
-          switch (this.props.tag) {
-            case 'comment':
-              this.props.updateCommentPageAction(index + 1, this.state.sizes);
-              break;
-            case 'video' :
-              this.props.updateVideoPageAction(index + 1, this.state.sizes);
-              break;
-            default :
-              break;
-          }
-          document.documentElement.scrollTop = 0;
-        };
-        return ((index + 1) === this.state.currPage) ? (
+        if (index - this.state.currPage >= -4&& index - this.state.currPage <= 2) {
+          return ((index + 1) === this.state.currPage) ? (
             <div key={index} className="index-text currPage-index-text">[{index + 1}]</div>
           ) : (
-            <div key={index} className="index-text" onClick={e => changePage(e)}>{index + 1}</div>
+            <div key={index} className="index-text" onClick={e => this.changePage(e, index)}>{index + 1}</div>
           );
+        } 
+        return null;
       });
+    } else {
+     pagination = (
+      <div key='0' className="index-text currPage-index-text">[1]</div>
+    );
     }
     return (
-      <div className="pagination">{pagination}</div>
+      <div className="pagination">
+        <div className="index-text" onClick={e => {
+          if(this.state.currPage > 1) {
+            this.changePage(e, this.state.currPage-2);
+          }
+        }}><GoChevronLeft /></div>
+        {pagination}
+        <div className="index-text"  onClick={e => {
+          const num = !pagination || pagination === null ? 1 : pagination.length;
+          if (this.state.currPage < num) {
+            this.changePage(e, this.state.currPage);
+          }
+        }}><GoChevronRight /></div>
+      </div>
     );
   }
 }
--- src/app/components/accessories/comment/CommentBlock.jsx
+++ src/app/components/accessories/comment/CommentBlock.jsx
@@ -1,18 +1,142 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { GoChevronDown, GoChevronUp } from 'react-icons/go';
 import { formatDateTime } from '../../../utils/dateTools';
+import { getSessionTokenJson } from '../../../api/apiHelper';
+import { getCommentListAction, completeGetComment } from '../../../actions/getCommentListAction';
+import { postComment } from '../../../api/comment';
 
 class CommentBlock extends React.Component {
+  state = {
+    isOpenReply: false,
+    isReplyArr: Array(this.props.commentInfo.length).fill(false)
+  }
+
+  //这个是comment的输入栏的提交法方法
+  handleReplyClick = (e, replyCom) => {
+    const comment = this.refs[`reply-${replyCom.commentId}`].value;
+    if (!comment || comment === null || comment === '' || (typeof comment === 'string' && comment.trim().length === 0)) {
+      alert('fill with something please...');
+      return;
+    }
+    this.refs[`reply-${replyCom.commentId}`].value = '';
+    const inputJson = {
+      commentContent: comment,
+      videoId: replyCom.videoId,
+      userId: getSessionTokenJson().user.userId,
+      commentParentId: replyCom.commentId
+    };
+    e.preventDefault();
+    postComment(inputJson)
+    .then(() => {
+      this.props.completeGetComment();    //先让commentList清零来重新加载commentlist
+      this.props.getCommentListAction({ videoId: replyCom.videoId });
+    })
+    .catch((err) => {
+      alert(`failed post comment${err}`);
+    });
+  };
+
   render() {
     const commentData = this.props.commentInfo;
     const rootComment = commentData[0];
     const uploadDate = formatDateTime(parseInt(rootComment.commentDate));
+    const rootReplayIndex = 0;
+    const replyInput = this.state.isReplyArr[rootReplayIndex] ? (
+      <div>
+        <input
+          className="reply-comment-input"
+          id={rootComment.id}
+          type="text"
+          ref={`reply-${rootComment.commentId}`}
+          placeholder="press enter to reply this comment"
+        />
+        <input 
+          className="reply-comment-confirm"
+          type="button"
+          value="reply"
+          onClick={e=>this.handleReplyClick(e, rootComment)}
+        />
+        <input 
+          className="reply-comment-cancel"
+          type="button"
+          value="cancel"
+          onClick={e=>{
+            e.preventDefault();
+            const newIsReplyArr = Array(this.props.commentInfo.length).fill(false);
+            this.setState({ isReplyArr: newIsReplyArr });
+          }}
+        />
+      </div>
+    ) : (
+      <span className="reply-btn" onClick={e=>{
+        const userJSON = getSessionTokenJson();
+        if (!userJSON) {
+          alert('please login or sign up a new account');
+          return 0;
+        }
+        const newIsReplyArr = Array(this.props.commentInfo.length).fill(false);
+        newIsReplyArr[rootReplayIndex] = true
+        this.setState({ isReplyArr: newIsReplyArr });
+        }}>
+        Reply
+      </span>
+    );
 
-    const replyComments = commentData;
-    replyComments.splice(0, 1);
-    const commentReply = replyComments.length > 0 ? replyComments.map((comment, index)=>{
+    const renderReplyList = this.state.isOpenReply ? commentData.map((comment, index)=>{
       const replayDate = formatDateTime(parseInt(comment.commentDate));
+      if(index===0) {
+        return (
+          <span className="reply-arro" onClick={e=>{
+            e.preventDefault();
+            this.setState({ isOpenReply: false });
+          }}>
+            <GoChevronUp /> hide reply
+          </span>
+        );
+      }
+      const replyInnerInput = this.state.isReplyArr[index] ? (
+        <div>
+          <input
+            className="reply-comment-input"
+            id={comment.id}
+            type="text"
+            ref={`reply-${comment.commentId}`}
+            placeholder="press enter to reply this comment"
+          />
+          <input 
+            className="reply-comment-confirm"
+            type="button"
+            value="reply"
+            onClick={e=>this.handleReplyClick(e, comment)}
+          />
+          <input 
+          className="reply-comment-cancel"
+          type="button"
+          value="cancel"
+          onClick={e=>{
+            e.preventDefault();
+            const newIsReplyArr = Array(this.props.commentInfo.length).fill(false);
+            this.setState({ isReplyArr: newIsReplyArr });
+          }}
+        />
+        </div>
+      ) : (
+        <span className="reply-btn" onClick={e=>{
+          const userJSON = getSessionTokenJson();
+          if (!userJSON) {
+            alert('please login or sign up a new account');
+            return 0;
+          }
+          const newIsReplyArr = Array(this.props.commentInfo.length).fill(false);
+          newIsReplyArr[index] = true
+        this.setState({ isReplyArr: newIsReplyArr });
+          }}>
+          Reply
+        </span>
+      ); 
       return (
-        <div key={index} className="comment-block-section">
+        <div key={index} className="comment-block-section reply">
           <div className="comment-user-avatar">
             <img 
                 src={comment.userInfo.userIcon}
@@ -30,9 +154,19 @@ class CommentBlock extends React.Component {
             <p>replay to {rootComment.userInfo.userName}</p>
             {comment.commentContent}
           </div>
+          <div className="reply-input-section">
+            {replyInnerInput}
+          </div>
         </div>
       );
-    }) : null;
+    }) : (
+      <span className="reply-arro" onClick={e=>{
+        e.preventDefault();
+        this.setState({ isOpenReply: true });
+      }}>
+        <GoChevronDown /> display reply
+      </span>
+    );
 
     return (
       <div className="comment-block-section">
@@ -52,12 +186,23 @@ class CommentBlock extends React.Component {
         <div className="comment-text">
           {rootComment.commentContent}
         </div>
+        <div className="reply-input-section">
+          {replyInput}
+        </div>
         <div className="comment-reply">
-        {commentReply}
+          {commentData.length === 1 ? null : renderReplyList}
         </div>
       </div>
     );
   }
 }
 
-export default  CommentBlock;
+const mapStateToProps = (state) => (state);
+
+export default connect(
+  mapStateToProps, {
+    getCommentListAction,
+    completeGetComment
+  }
+)(CommentBlock);
+
