2323db0061c74b8fd6891ad9049ab7d3daba639f
billtong <zhiyuantongbill@gmail.com>
2019-07-04 02:53:16 -0400

    fix url

--- src/utils/api/baseURL.js
+++ src/utils/api/baseURL.js
@@ -1,8 +1,8 @@
-export const BASE_URL = 'http://localhost:8080/empty-server-1.0.0/';
-export const BASE_MULTIPARTFILES_URL = 'http://localhost:8080/empty-video-files/'
+//export const BASE_URL = 'http://localhost:8080/empty-server-1.0.0/';
+//export const BASE_MULTIPARTFILES_URL = 'http://localhost:8080/empty-video-files/'
 
 //digitalOcean server
-///export const BASE_URL = 'http://178.128.236.114:8080/empty-server-1.0.0/';
+export const BASE_URL = 'http://178.128.236.114:8080/empty-server-1.0.0/';
 
 //static resources
-//export const BASE_MULTIPARTFILES_URL = 'http://178.128.236.114:8080/empty-video-files/';
+export const BASE_MULTIPARTFILES_URL = 'http://178.128.236.114:8080/empty-video-files/';
